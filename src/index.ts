44c3921de345561c9ce82afc2eda7f9ce1297784
Vitor Vilela <vilelavitor305@gmail.com>
2023-04-22 16:44:18 -0300

    Banco de Dados conectado (MySQL)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import mysql from 'mysql'
 import { Request, Response} from 'express'
 import bodyParser, { json } from 'body-parser';
 
@@ -8,7 +9,48 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 app.get('/:id', (req: Request, res: Response) => {
-    res.send("Hello World!");
+    
+    var pool = mysql.createPool({
+        host: '127.0.0.1',
+        user: 'root',
+        password: 'root123',
+        database: 'testapi',
+    })
+    
+    pool.getConnection((err: any, conn: any)=>{
+        if(err){
+            console.log('Entered into error')
+            console.log(err)
+            res.send({
+                sucess: false,
+                statusCode: 500,
+                message: 'Getting error during the connection'
+            })
+
+            return;
+        }
+
+        console.log('The id: '+ req.params.id)
+
+        conn.query('SELECT * from users where id=?', [req.params.id], (err: any, rows: any)=>{
+            if(err){
+                conn.release()
+                return res.send({
+                    sucess: false,
+                    statusCode: 400
+                });
+            }
+
+            res.send({
+                message: 'Sucess',
+                statusCode: 200,
+                data: rows
+            });
+
+            //Close the connection
+            conn.release()
+        })
+    })
 });
 
 app.post('/', (req: Request, res: Response)=>{
